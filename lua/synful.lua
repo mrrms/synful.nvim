134eaa8115fa83a4518b9652039fe8f02e8211d3
rmsackma <rmsackma@icloud.com>
2022-10-03 13:40:08 -0600

    modifying continues

--- lua/synful.lua
+++ lua/synful.lua
@@ -9,38 +9,39 @@ local function highlight(group, styles)
   vim.api.nvim_command('highlight ' .. group .. ' ' .. gui .. ' ' .. sp .. ' ' .. fg .. ' ' .. bg)
 end
 
-local bg_darker      = '#111119'
-local bg_dark        = '#151725'
-local bg             = '#292d3e'
-local bg_light       = '#32374d'
-local bg_lighter     = '#454575'
-local grey           = '#8585AA'
-local red            = '#9C4F63'
-local heavy_red      = '#D20434'
-local green          = '#5F875F'
-local blue           = '#82aaff'
-local yellow         = '#ebcb8b'
-local orange         = '#DB9280'
-local purple         = '#BB91ED'
-local cyan           = '#7AA7FF'
-local fg             = '#a8b4f6'
-local fg_light       = '#B5B5E3'
-local fg_dark        = '#676e96'
-local hollow         = '#565E99'
-local hollow_lighter = '#6C7DD0'
-local white          = '#C0C0FC'
+local bg_darker   = '#111119'
+local bg_dark     = '#151725'
+local bg          = '#292d3e'
+local bg_light    = '#32374d'
+local bg_lighter  = '#454575'
+local grey        = '#8585AA'
+local red         = '#9C4F63'
+local heavy_red   = '#D20434'
+local green       = '#5F875F'
+local light_green = '#96C0C0'
+local pale_blue   = '#7AA7FF'
+local blue        = '#6C7DD0'
+local deep_blue   = '#445599'
+local yellow      = '#ebcb8b'
+local orange      = '#DB9280'
+local purple      = '#BB91ED'
+local fg          = '#a8b4f6'
+local fg_light    = '#B5B5E3'
+local fg_dark     = '#676e96'
+local hollow      = '#565E99'
+local white       = '#C0C0FC'
 
 --> Editor Highlight
 local editor_syntax = {
   CursorLine   = { bg = bg },
   Cursor       = { fg = bg_dark, bg = yellow },
-  Directory    = { fg = blue, gui = 'bold' },
+  Directory    = { fg = deep_blue, gui = 'bold' },
   DiffAdd      = { fg = green },
   DiffChange   = { fg = yellow },
   DiffDelete   = { fg = red },
   DiffText     = { fg = blue },
   EndOfBuffer  = { bg = bg_dark, fg = bg_dark },
-  ErrorMsg     = { fg = red, gui = 'bold' },
+  ErrorMsg     = { fg = heavy_red, gui = 'bold' },
   VertSplit    = { bg = bg_dark, fg = bg },
   Folded       = { fg = fg_dark, gui = 'italic' },
   FoldColumn   = { fg = yellow },
@@ -49,11 +50,11 @@ local editor_syntax = {
   Substitute   = { bg = blue, fg = bg },
   LineNr       = { fg = fg_dark },
   CursorLineNr = { fg = fg },
-  MatchParen   = { fg = cyan, gui = 'bold' },
-  Normal       = { fg = fg_light, bg = bg_darker },
-  NormalFloat  = { bg = bg_darker },
-  Pmenu        = { bg = bg_darker, fg = fg_light },
-  PmenuSel     = { bg = cyan, fg = bg_lighter, gui = 'bold' },
+  MatchParen   = { fg = heavy_red, gui = 'bold' },
+  Normal       = { fg = fg_light }, --> no background colour permits the shell's default
+  NormalFloat  = {},
+  Pmenu        = { fg = fg_dark },
+  PmenuSel     = { bg = deep_blue, fg = pale_blue, gui = 'bold' },
   PmenuSbar    = { bg = bg_lighter },
   PmenuThumb   = { bg = fg },
   Search       = { bg = hollow },
@@ -64,10 +65,10 @@ local editor_syntax = {
   SpellRare    = { gui = 'underline', sp = blue },
   TabLine      = { bg = bg, fg = fg_light },
   TabLineFill  = { bg = bg, fg = fg_light },
-  TabLineSel   = { bg = cyan, fg = bg_dark, gui = 'bold' },
+  TabLineSel   = { bg = blue, fg = bg_dark, gui = 'bold' },
   Title        = { fg = green },
-  Visual       = { bg = hollow_lighter },
-  VisualNOS    = { bg = hollow_lighter },
+  Visual       = { bg = deep_blue },
+  VisualNOS    = { bg = deep_blue },
   WarningMsg   = { fg = yellow, gui = 'italic' },
   Whitespace   = { bg = yellow }, -- TODO: i don't know where this is
 
@@ -81,7 +82,7 @@ local editor_syntax = {
   gitcommitNoBranch       = { fg = purple },
   gitcommitDiscardedType  = { fg = red },
   gitcommitSelectedType   = { fg = green },
-  gitcommitUntrackedFile  = { fg = cyan },
+  gitcommitUntrackedFile  = { fg = blue },
   gitcommitDiscardedFile  = { fg = red },
   gitcommitDiscardedArrow = { fg = red },
   gitcommitSelectedFile   = { fg = green },
@@ -115,25 +116,25 @@ end
 --> Vim Default Code Syntax
 local code_syntax = {
   Comment   = { fg = fg_dark, gui = 'italic' },
-  Constant  = { fg = cyan },
-  String    = { fg = green },
+  Constant  = { fg = deep_blue },
+  String    = { fg = light_green, gui = 'italic' },
   Character = { fg = green, gui = 'bold' },
   Number    = { fg = orange },
   Float     = { fg = orange },
   Boolean   = { fg = orange },
 
-  Identifier = { fg = heavy_red },
-  Function   = { fg = cyan, gui = 'italic' },
+  Identifier = { fg = green },
+  Function   = { fg = deep_blue, gui = 'italic' },
 
-  Statement   = { fg = blue, gui = 'italic' },
-  Conditional = { fg = cyan, gui = 'italic' },
-  Repeat      = { fg = cyan, gui = 'italic' },
-  Label       = { fg = cyan, gui = 'italic' },
-  Exception   = { fg = cyan, gui = 'italic' },
-  Operator    = { fg = cyan },
-  Keyword     = { fg = heavy_red },
+  Statement   = { fg = deep_blue, gui = 'italic' },
+  Conditional = { fg = deep_blue, gui = 'italic' },
+  Repeat      = { fg = deep_blue, gui = 'italic' },
+  Label       = { fg = deep_blue, gui = 'italic' },
+  Exception   = { fg = deep_blue, gui = 'italic' },
+  Operator    = { fg = deep_blue },
+  Keyword     = { fg = deep_blue },
 
-  Include   = { fg = blue },
+  Include   = { fg = deep_blue },
   Define    = { fg = purple },
   Macro     = { fg = purple },
   PreProc   = { fg = yellow },
@@ -177,11 +178,11 @@ end
 
 --> Syntax Plugin And Language Highlight Groups
 local lang_syntax = {
-  xmlEndTag         = { fg = cyan, gui = 'italic' },
+  xmlEndTag         = { fg = blue, gui = 'italic' },
   -- lua.vim
   luaTable          = { fg = fg_light },
-  luaBraces         = { fg = cyan },
-  luaIn             = { fg = cyan, gui = 'italic' },
+  luaBraces         = { fg = bg_light },
+  luaIn             = { fg = blue, gui = 'italic' },
   -- lua polyglot (tbastos/vim-lua)
   luaFunc           = { fg = blue },
   luaFuncCall       = { fg = blue },
@@ -190,8 +191,8 @@ local lang_syntax = {
   luaLocal          = { fg = purple },
   luaSpecialValue   = { fg = purple },
   luaStatement      = { fg = purple },
-  luaFunction       = { fg = cyan, gui = 'italic' },
-  luaSymbolOperator = { fg = cyan },
+  luaFunction       = { fg = blue, gui = 'italic' },
+  luaSymbolOperator = { fg = blue },
   luaConstant       = { fg = orange },
 
   -- zsh.vim
@@ -199,23 +200,23 @@ local lang_syntax = {
   zshComment         = code_syntax.Comment,
   zshPreProc         = code_syntax.PreProc,
   zshString          = code_syntax.String,
-  zshStringDelimiter = { fg = cyan },
+  zshStringDelimiter = { fg = blue },
   zshPrecommand      = { fg = blue },
   zshKeyword         = code_syntax.Function,
   zshCommands        = { fg = blue },
   zshOptStart        = { fg = blue, gui = 'italic' },
-  zshOption          = { fg = cyan, gui = 'italic' },
+  zshOption          = { fg = blue, gui = 'italic' },
   zshNumber          = code_syntax.Number,
   zshSubst           = { fg = yellow },
-  zshSubstDelim      = { fg = cyan },
+  zshSubstDelim      = { fg = blue },
 
   -- rust polyglot (rust.vim)
   rustKeyword     = { fg = orange },
   rustFuncCall    = { fg = blue },
-  rustModPathSep  = { fg = cyan },
+  rustModPathSep  = { fg = blue },
   rustIdentifier  = { fg = fg_light },
   rustFuncName    = { fg = blue },
-  rustSigil       = { fg = cyan },
+  rustSigil       = { fg = blue },
   rustMacro       = { fg = blue },
   rustStorage     = { fg = orange },
   rustModPath     = { fg = fg_light },
@@ -225,72 +226,72 @@ local lang_syntax = {
 
 
   -- javascript polyglot (pangloss/vim-javascript)
-  jsFunction            = { fg = cyan, gui = 'italic' },
+  jsFunction            = { fg = blue, gui = 'italic' },
   jsFuncName            = { fg = blue },
-  jsImport              = { fg = cyan, gui = 'italic' },
-  jsFrom                = { fg = cyan, gui = 'italic' },
+  jsImport              = { fg = blue, gui = 'italic' },
+  jsFrom                = { fg = blue, gui = 'italic' },
   jsStorageClass        = { fg = purple },
-  jsAsyncKeyword        = { fg = cyan, gui = 'italic' },
-  jsForAwait            = { fg = cyan, gui = 'italic' },
+  jsAsyncKeyword        = { fg = blue, gui = 'italic' },
+  jsForAwait            = { fg = blue, gui = 'italic' },
   jsArrowFunction       = { fg = purple },
   jsReturn              = { fg = purple },
   jsFuncCall            = { fg = blue },
-  jsFuncBraces          = { fg = cyan },
-  jsExport              = { fg = cyan, gui = 'italic' },
+  jsFuncBraces          = { fg = bg_light },
+  jsExport              = { fg = blue, gui = 'italic' },
   jsGlobalObjects       = { fg = yellow },
   jsxTagName            = { fg = red },
   jsxComponentName      = { fg = yellow },
   jsxAttrib             = { fg = purple },
-  jsxBraces             = { fg = cyan },
-  jsTemplateBraces      = { fg = cyan },
-  jsFuncParens          = { fg = cyan },
-  jsDestructuringBraces = { fg = cyan },
-  jsObjectBraces        = { fg = cyan },
-  jsObjectKey           = { fg = red },
+  jsxBraces             = { fg = bg_light },
+  jsTemplateBraces      = { fg = bg_light },
+  jsFuncParens          = { fg = bg_light },
+  jsDestructuringBraces = { fg = bg_light },
+  jsObjectBraces        = { fg = bg_light },
+  jsObjectKey           = { fg = pale_blue },
   jsObjectShorthandProp = { fg = fg_light },
   jsNull                = { fg = orange },
 
-  typescriptOperator          = { fg = cyan },
-  typescriptAsyncFuncKeyword  = { fg = cyan, gui = 'italic' },
+  typescriptOperator          = { fg = blue },
+  typescriptAsyncFuncKeyword  = { fg = blue, gui = 'italic' },
   typescriptCall              = { fg = fg_light },
-  typescriptBraces            = { fg = cyan },
-  typescriptTemplateSB        = { fg = cyan },
-  typescriptTry               = { fg = cyan, gui = 'italic' },
-  typescriptExceptions        = { fg = cyan, gui = 'italic' },
-  typescriptOperator          = { fg = cyan, gui = 'italic' },
-  typescriptExport            = { fg = cyan, gui = 'italic' },
-  typescriptStatementKeyword  = { fg = cyan, gui = 'italic' },
-  typescriptImport            = { fg = cyan, gui = 'italic' },
+  typescriptBraces            = { fg = bg_light },
+  typescriptTemplateSB        = { fg = blue },
+  typescriptTry               = { fg = blue, gui = 'italic' },
+  typescriptExceptions        = { fg = blue, gui = 'italic' },
+  typescriptOperator          = { fg = blue, gui = 'italic' },
+  typescriptExport            = { fg = blue, gui = 'italic' },
+  typescriptStatementKeyword  = { fg = blue, gui = 'italic' },
+  typescriptImport            = { fg = blue, gui = 'italic' },
   typescriptArrowFunc         = { fg = purple },
   typescriptArrowFuncArg      = { fg = fg_light },
   typescriptArrayMethod       = { fg = blue },
   typescriptStringMethod      = { fg = blue },
   typescriptTypeReference     = { fg = yellow },
   typescriptObjectLabel       = { fg = red },
-  typescriptParens            = { fg = fg_light },
-  typescriptTypeBrackets      = { fg = cyan },
+  typescriptParens            = { fg = bg_light },
+  typescriptTypeBrackets      = { fg = blue },
   typescriptXHRMethod         = { fg = blue },
   typescriptResponseProp      = { fg = blue },
   typescriptBOMLocationMethod = { fg = blue },
   typescriptHeadersMethod     = { fg = blue },
   typescriptVariable          = { fg = purple },
 
-  htmlTag = { fg = cyan },
-  htmlEndTag = { fg = cyan },
+  htmlTag = { fg = blue },
+  htmlEndTag = { fg = blue },
 }
 
 for group, styles in pairs(lang_syntax) do
   highlight(group, styles)
 end
 
---> Setting Neovim Terminal Color {{{
+--> Setting Neovim Terminal Color
 vim.g.terminal_color_0          = bg_dark
 vim.g.terminal_color_1          = red
-vim.g.terminal_color_2          = green
+vim.g.terminal_color_2          = light_green
 vim.g.terminal_color_3          = yellow
 vim.g.terminal_color_4          = blue
 vim.g.terminal_color_5          = purple
-vim.g.terminal_color_6          = cyan
+vim.g.terminal_color_6          = blue
 vim.g.terminal_color_7          = fg
 vim.g.terminal_color_8          = grey
 vim.g.terminal_color_9          = red
@@ -298,7 +299,7 @@ vim.g.terminal_color_10         = green
 vim.g.terminal_color_11         = orange
 vim.g.terminal_color_12         = blue
 vim.g.terminal_color_13         = purple
-vim.g.terminal_color_14         = cyan
+vim.g.terminal_color_14         = blue
 vim.g.terminal_color_15         = white
 vim.g.terminal_color_background = bg_dark
 vim.g.terminal_color_foreground = fg_light
