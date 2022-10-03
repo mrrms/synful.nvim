821452c0d810001865019ed63bf35676d13f1bf1
rmsackma <rmsackma@icloud.com>
2022-10-03 09:46:43 -0600

    modifying continues

--- colors/synful.lua
+++
@@ -1,9 +0,0 @@
-package.loaded["synful"] = nil
-package.loaded["synful.util"] = nil
-package.loaded["synful.groups"] = nil
-
-require("synful").apply()
-
-vim.g.colors_name = "synful"
-
---> https://youtu.be/otqz4uCyCAI?list=PLOe6AggsTaVvjY25kxHlGccbYfe7hSqtp&t=22
---
+++ colors/synful.vim
@@ -0,0 +1,8 @@
+hi clear
+syntax reset
+set t_Co=256
+let g:colors_name="synful"
+
+lua require 'synful'
+
+set background=dark
