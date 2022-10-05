5aba0d870c5c997ac437862eb7854e649a1de2b8
mrrms <rmsackma@icloud.com>
2022-10-05 11:43:17 -0600

    modifying continues

--- lua/synful.lua
+++ lua/synful.lua
@@ -52,7 +52,7 @@ local editor_syntax = {
   MatchParen   = { fg = rouge, gui = 'bold' },
   Normal       = { fg = fg_light }, --> no background colour permits the shell's default
   NormalFloat  = {},
-  Pmenu        = { fg = fg, bg = 'NONE' },
+  Pmenu        = { fg = fg, bg = bg_dark },
   PmenuSel     = { bg = bg_lighter, fg = pale_blue },
   PmenuSbar    = { bg = bg },
   PmenuThumb   = { bg = bg_lighter },
