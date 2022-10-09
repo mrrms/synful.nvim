0e4914911f118bc34922792aec4f5685e991566f
mrrms <rmsackma@icloud.com>
2022-10-09 15:59:01 -0600

    ongoing modifications

--- lua/synful.lua
+++ lua/synful.lua
@@ -52,7 +52,7 @@ local editor_syntax = {
   Normal       = { fg = fg_light },
   NormalFloat  = {},
   Pmenu        = { fg = fg, bg = zero_dark },
-  PmenuSel     = { fg = white, bg = light_green },
+  PmenuSel     = { fg = white, bg = dead_grey },
   PmenuSbar    = { fg = fg },
   PmenuThumb   = { fg = bruise },
   Search       = { fg = white, bg = hollow },
