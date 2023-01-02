569eecca85c3f09af7b7c6594bd578a0b4339a26
mrrms <rmsackma@icloud.com>
2023-01-02 08:08:16 -0700

    modify

--- lua/synful.lua
+++ lua/synful.lua
@@ -6,7 +6,7 @@ local function highlight(group, styles)
   vim.api.nvim_command('highlight ' .. group .. ' ' .. gui .. ' ' .. sp .. ' ' .. fg .. ' ' .. bg)
 end
 
-local zero_dark  = '#000000'
+local zero_dark  = '#1E1E2E'
 local deep_blue  = '#121224'
 local death      = '#505075'
 local pale_blue  = '#7AA7FF'
@@ -430,3 +430,30 @@ vim.g.terminal_color_14         = lagoon
 vim.g.terminal_color_15         = icicle
 vim.g.terminal_color_background = death
 vim.g.terminal_color_foreground = white
+
+local color_palette = {
+  rosewater = "#F5E0DC",
+  flamingo = "#F2CDCD",
+  pink = "#F5C2E7",
+  mauve = "#CBA6F7",
+  red = "#F38BA8",
+  maroon = "#EBA0AC",
+  peach = "#FAB387",
+  yellow = "#F9E2AF",
+  green = "#A6E3A1",
+  teal = "#94E2D5",
+  sky = "#89DCEB",
+  blue = "#90C1FB",
+  sapphire = "#74C7EC",
+  lavender = "#B4BEFE",
+  text = "#C6D0F5",
+  overlay2 = "#AEB7D9",
+  overlay1 = "#969DBC",
+  overlay0 = "#7E84A0",
+  surface2 = "#666A83",
+  surface1 = "#4E5167",
+  surface0 = "#36374A",
+  base2 = "#1E1E2E",
+  base1 = "#12121C",
+  base0 = "#07070A",
+}
