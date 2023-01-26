08dea5e0f0e832874077400b021e740f989b8af4
mrrms <mrrms@icloud.com>
2023-01-26 09:49:40 -0700

    ongoing modification

--- lua/synful.lua
+++ lua/synful.lua
@@ -20,7 +20,7 @@ local yellow     = '#F2CDCD'
 local orange     = '#DB9280'
 local clover     = '#597a69'
 local green      = '#5F875F'
-local pale_green = '#89b0b1'
+local pale_green = '#9EBFBF'
 local grey       = '#8585AA'
 local freeze     = '#9FA7CF'
 
