5151d5fbd64fdbc1577ce54a37af1075d56ba29a
mrrms <mrrms@icloud.com>
2023-02-08 15:32:23 -0700

    ongoing

--- colors/synful.vim
+++ colors/synful.vim
@@ -1,7 +1,6 @@
 hi clear
 syntax reset
 set t_Co=256
-
 let g:colors_name='synful'
 
 lua require 'synful'
--- lua/synful.lua
+++ lua/synful.lua
@@ -1,4 +1,21 @@
-local hue = require 'synful.colour'
+local hue = {
+  space = '#111119',
+  raven = '#1E202D',
+  metal = '#3B4267',
+  steel = '#405080',
+  royal = '#6070C1',
+  capri = '#7AA7FF',
+  ultra = '#CBA6F7',
+  mauve = '#734a80',
+  rouge = '#A94064',
+  peach = '#FFCDCD',
+  amber = '#DB9280',
+  bazil = '#597a69',
+  olive = '#5F875F',
+  paris = '#9EBFBF',
+  slate = '#676E96',
+  pearl = '#9FA7CF',
+}
 
 local editor_syntax = {
   LineNr                  = { fg = hue.metal },
