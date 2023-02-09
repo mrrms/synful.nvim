8b35a2dd9d767db19578e7dd39df11331dfe5797
mrrms <mrrms@icloud.com>
2023-02-08 20:21:58 -0700

    ongoing

--- lua/synful/init.lua
+++ lua/synful/init.lua
@@ -1,7 +1,7 @@
 local groups = require 'synful.groups'
 
 return {
-  load = function(group, styles)
+  load = function()
     if vim.g.colors_name then vim.api.nvim_command 'hi clear' end
 
     vim.g.colors_name = 'synful'
@@ -11,5 +11,6 @@ return {
     for group, styles in pairs(groups) do
       vim.api.nvim_set_hl(0, tostring(group), styles)
     end
+
   end
 }
