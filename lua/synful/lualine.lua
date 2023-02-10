2b02db71b74c31bdac85cb8dae237ae2cc51b281
mrrms <mrrms@icloud.com>
2023-02-10 12:56:29 -0700

    lualine

--- lua/synful/lualine.lua
+++ lua/synful/lualine.lua
@@ -1,23 +1,23 @@
-local colour = require 'synful.colour'
+-- local colour = require 'synful.colour'
 
-local synful = {
-  normal = {
-    a = { fg = colour.steel, bg = colour.raven, gui = 'bold' },
-    b = { fg = colour.ultra, bg = colour.metal, gui = 'bold' },
-    c = { fg = colour.metal, bg = colour.space },
-  },
-  insert = { a = { fg = colour.pearl, bg = colour.metal, gui = 'bold' } },
-  replace = { a = { fg = colour.steel, bg = colour.pearl, gui = 'bold' } },
-  visual = { a = { fg = colour.pearl, bg = colour.mauve, gui = 'bold' } },
-  inactive = {
-    a = { fg = colour.pearl, bg = colour.rouge },
-    b = { fg = colour.pearl, bg = colour.rouge },
-    c = { fg = colour.pearl, bg = colour.rouge },
-  }
-}
+-- local synful = {
+--   normal = {
+--     a = { fg = colour.steel, bg = colour.raven, gui = 'bold' },
+--     b = { fg = colour.ultra, bg = colour.metal, gui = 'bold' },
+--     c = { fg = colour.metal, bg = colour.space },
+--   },
+--   insert = { a = { fg = colour.pearl, bg = colour.metal, gui = 'bold' } },
+--   replace = { a = { fg = colour.steel, bg = colour.pearl, gui = 'bold' } },
+--   visual = { a = { fg = colour.pearl, bg = colour.mauve, gui = 'bold' } },
+--   inactive = {
+--     a = { fg = colour.pearl, bg = colour.rouge },
+--     b = { fg = colour.pearl, bg = colour.rouge },
+--     c = { fg = colour.pearl, bg = colour.rouge },
+--   }
+-- }
 
-require('lualine').setup {
-  options = {
-    theme = synful
-  }
-}
+-- require('lualine').setup {
+--   options = {
+--     theme = synful
+--   }
+-- }
