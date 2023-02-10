836ef9682cc44ad5a58274edd047000de662ce6e
mrrms <mrrms@icloud.com>
2023-02-09 20:21:50 -0700

    lualine maybe

---
+++ lua/synful/lualine.lua
@@ -0,0 +1,17 @@
+local colour = require 'synful.colour'
+
+return {
+  normal = {
+    a = { fg = colour.steel, bg = colour.raven, gui = 'bold' },
+    b = { fg = colour.ultra, bg = colour.metal, gui = 'bold' },
+    c = { fg = colour.metal, bg = colour.space },
+  },
+  insert = { a = { fg = colour.pearl, bg = colour.metal, gui = 'bold' } },
+  replace = { a = { fg = colour.steel, bg = colour.pearl, gui = 'bold' } },
+  visual = { a = { fg = colour.pearl, bg = colour.mauve, gui = 'bold' } },
+  inactive = {
+    a = { fg = colour.pearl, bg = colour.rouge },
+    b = { fg = colour.pearl, bg = colour.rouge },
+    c = { fg = colour.pearl, bg = colour.rouge },
+  }
+}
