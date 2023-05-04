a7976dfd85d5d7b27ed97039ca28f6fb4f142e31
mrrms <mrrms@icloud.com>
2023-05-04 17:02:35 -0600

    resolves semantic highlighting for lsp servers

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -452,19 +452,27 @@ return {
   jsxTagName                          = { link = 'keyword' },
   luaFuncCall                         = { link = 'keyword' },
   typescriptTry                       = { link = 'keyword' },
-  LazyNormal                          = { link = 'pmenu' },
-  ['@lsp.type.class']                 = { link = '@type' },
-  ['@lsp.type.decorator']             = { link = '@function' },
-  ['@lsp.type.enum']                  = { link = '@type' },
-  ['@lsp.type.enumMember']            = { link = '@constant' },
-  ['@lsp.type.function']              = { link = '@function' },
-  ['@lsp.type.interface']             = { link = '@type' },
-  ['@lsp.type.macro']                 = { link = '@macro' },
-  ['@lsp.type.method']                = { link = '@method' },
-  ['@lsp.type.namespace']             = { link = '@namespace' },
-  ['@lsp.type.parameter']             = { link = '@parameter' },
-  ['@lsp.type.property']              = { link = '@property' },
-  ['@lsp.type.struct']                = { link = '@structure' },
-  ['@lsp.type.type']                  = { link = '@type' },
-  ['@lsp.type.variable']              = { link = '@variable' },
+  LazyNormal                          = { link = 'pmenu' }
 }
+
+-- resolves semantic highlighting for lsp servers
+local links = {
+  ['@lsp.type.namespace'] = '@namespace',
+  ['@lsp.type.type'] = '@type',
+  ['@lsp.type.class'] = '@type',
+  ['@lsp.type.enum'] = '@type',
+  ['@lsp.type.interface'] = '@type',
+  ['@lsp.type.struct'] = '@structure',
+  ['@lsp.type.parameter'] = '@parameter',
+  ['@lsp.type.variable'] = '@variable',
+  ['@lsp.type.property'] = '@property',
+  ['@lsp.type.enumMember'] = '@constant',
+  ['@lsp.type.function'] = '@function',
+  ['@lsp.type.method'] = '@method',
+  ['@lsp.type.macro'] = '@macro',
+  ['@lsp.type.decorator'] = '@function',
+}
+
+for newgroup, oldgroup in pairs(links) do
+  vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+end
