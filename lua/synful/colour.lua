4566149cd2b0096db89c5c206f06056f4e94a9ac
mrrms <mrrms@icloud.com>
2023-06-11 15:59:41 -0600

    Alter metal colour (more metally)

--- lua/synful/colour.lua
+++ lua/synful/colour.lua
@@ -1,7 +1,7 @@
 return {
   space = '#111119',
   raven = '#212131',
-  metal = '#4F5576',
+  metal = '#3e435c',
   steel = '#405080',
   royal = '#6070C1',
   capri = '#7AA7FF',
--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -29,7 +29,7 @@ return {
   Character                           = { fg = colour.ultra },
   PreProc                             = { fg = colour.slate },
   Conceal                             = { fg = colour.paris },
-  CursorLine                          = { bg = colour.space },
+  CursorLine                          = { bg = colour.raven },
   FloatBorder                         = { fg = colour.raven },
   LineNr                              = { fg = colour.raven },
   Operator                            = { fg = colour.metal },
