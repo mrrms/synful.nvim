5551a45b1b317ae71815399f060e47731f46fbff
mrrms <mrrms@icloud.com>
2023-07-08 14:54:16 -0600

    iterm2 integrations

--- lua/synful/colour.lua
+++ lua/synful/colour.lua
@@ -9,13 +9,19 @@ return {
   mauve = '#734a80',
   rouge = '#A94064',
   amber = '#DB9280',
-  blush = '#24030e',
+  blush = '#2F1D23',
   peach = '#FFCDCD',
-  orang = '#36130b',
+  orang = '#342420',
   bazil = '#597a69',
   olive = '#5F875F',
-  green = '#0f2424',
+  green = '#2D3D3D',
   paris = '#9EBFBF',
   slate = '#676E96', -->#8088b0
+  stone = '#323750', 
   pearl = '#9FA7CF', -->#A8B4F6
 }
+
+blush
+green
+raven
+orang
