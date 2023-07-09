50f27bf0761cb4d76c884cb590363287effbed56
mrrms <mrrms@icloud.com>
2023-07-09 11:56:01 -0600

    Cursorline background colour counter beneficial

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -58,7 +58,7 @@ return {
   StatusLineNC                        = { fg = colour.space, bg = 'NONE' },
   Visual                              = { fg = colour.capri, bg = colour.steel, bold = true },
   Search                              = { fg = colour.ultra, bg = colour.mauve, bold = true },
-  CursorLineNr                        = { fg = colour.bazil, bold = true },
+  CursorLineNr                        = { fg = colour.bazil, bg = 'NONE', bold = true },
   PmenuSel                            = { fg = colour.slate, bg = colour.raven, bold = true },
   Comment                             = { fg = colour.metal, italic = true },
   jsOperatorKeyword                   = { fg = colour.bazil, italic = true, bold = true },
