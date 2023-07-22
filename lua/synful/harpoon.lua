60050083ebcec725b1782e968d24a5d61e11350b
mrrms <mrrms@icloud.com>
2023-07-22 12:01:55 -0600

    harpoon

--- lua/synful/harpoon.lua
+++ lua/synful/harpoon.lua
@@ -1,9 +1,11 @@
 local colour = require('synful.colour')
 
 return {
-  TabLineFill = { fg = colour.pearl },
-  HarpoonActive = { fg = colour.amber, bg = 'NONE', bold = true },
-  HarpoonInactive = { fg = colour.stone },
-  HarpoonNumberActive = { fg = colour.space, bg = 'NONE' },
+  TabLine               = { fg = colour.stone, bg = 'NONE' },
+  TabLineSel            = { fg = colour.amber, bg = 'NONE', bold = true },
+  TabLineFill           = { fg = colour.pearl },
+  HarpoonActive         = { fg = colour.amber, bg = 'NONE', bold = true },
+  HarpoonInactive       = { fg = colour.stone },
+  HarpoonNumberActive   = { fg = colour.space, bg = 'NONE' },
   HarpoonNumberInactive = { fg = colour.space, bg = 'NONE' },
 }
