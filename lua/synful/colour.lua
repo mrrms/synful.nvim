5be11556881270d506f9ff9b6c9fe0a9afda8a98
mrrms <mrrms@icloud.com>
2023-07-31 12:55:09 -0600

    the pearl colour used for Normal is a little too bright.

--- lua/synful/colour.lua
+++ lua/synful/colour.lua
@@ -9,8 +9,8 @@ return {
   mauve = '#734a80',
   rouge = '#A94064',
   amber = '#DB9280',
-  blush = '#2F1D23',
   peach = '#FFCDCD',
+  blush = '#2F1D23',
   orang = '#25201E',
   bazil = '#597a69',
   olive = '#5F875F',
@@ -18,5 +18,5 @@ return {
   green = '#2D3D3D',
   slate = '#676E96', -->#8088b0
   stone = '#323750',
-  pearl = '#9FA7CF', -->#A8B4F6
+  pearl = '#747a99', --> #9FA7CF #A8B4F6
 }
