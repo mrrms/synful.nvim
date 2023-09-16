29221cae7d6023048de336130b483ad25df1911c
mrrms <mrrms@icloud.com>
2023-09-16 15:51:35 -0600

    changed init function

--- lua/synful/harpoon.lua
+++ lua/synful/harpoon.lua
@@ -1,9 +1,8 @@
 local colour = require('synful.colour')
 
--- harpoon links these groups to tabline & tablinesel
 return {
   HarpoonActive         = { fg = colour.slate, bg = 'NONE', gui = 'bold' },
   HarpoonInactive       = { fg = colour.stone, bg = 'NONE' },
-  HarpoonNumberActive   = { fg = colour.space, bg = 'NONE' },
+  HarpoonNumberActive   = { fg = colour.ultra, bg = 'NONE' },
   HarpoonNumberInactive = { fg = colour.space, bg = 'NONE' },
 }
