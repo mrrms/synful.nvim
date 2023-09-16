662a1a7ce20365d2604b6019e8f4b0c6dcda40a7
mrrms <mrrms@icloud.com>
2023-09-16 15:48:25 -0600

    changed init function

--- lua/synful/init.lua
+++ lua/synful/init.lua
@@ -1,25 +1,22 @@
-local M = {}
-
 local groups = require('synful.groups')
 local harpoon = require('synful.harpoon')
 
 require('synful.lualine')
 
-function _Highlight(opts)
-  for group, styles in pairs(opts) do
-    vim.api.nvim_set_hl(0, tostring(group), styles)
+return {
+  load = function()
+    if vim.g.colors_name then
+      vim.api.nvim_command('hi clear')
+    end
+
+    vim.g.colors_name = 'synful'
+
+    for group, styles in pairs(groups) do
+      vim.api.nvim_set_hl(0, tostring(group), styles)
+    end
+
+    for group, styles in pairs(harpoon) do
+      vim.api.nvim_set_hl(0, tostring(group), styles)
+    end
   end
-end
-
-function M.load()
-  if vim.g.colors_name then
-    vim.api.nvim_command('hi clear')
-  end
-
-  vim.g.colors_name = 'synful'
-
-  _Highlight(groups)
-  _Highlight(harpoon)
-end
-
-return M
+}
