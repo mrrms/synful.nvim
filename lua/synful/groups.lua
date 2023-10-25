a4e2cf7ea7145d72f2f186cdd9365ec733ea5e07
mrrms <mrrms@icloud.com>
2023-10-25 11:30:24 -0600

    Pearl reserection; Chore: reorganizing; Float background darker

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -37,7 +37,7 @@ return {
   Normal                              = { fg = colour.pearl },
   Tag                                 = { fg = colour.amber },
   Todo                                = { fg = colour.capri },
-  FloatBorder                         = { fg = 'NONE', bg = 'NONE' },
+  FloatBorder                         = { fg = colour.space, bg = 'NONE' },
   NonText                             = { fg = 'NONE', bg = 'NONE' },
   DiagnosticUnnecessary               = { fg = colour.ultra, bold = true },
   Function                            = { fg = colour.pearl, bold = true },
