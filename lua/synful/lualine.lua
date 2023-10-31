33b90948b7a98012316ac70cdde9c657c2fac57c
mrrms <mrrms@icloud.com>
2023-10-31 16:21:20 -0600

    Stone colour darkened

--- lua/synful/lualine.lua
+++ lua/synful/lualine.lua
@@ -9,7 +9,7 @@ lualine.setup({
       normal = {
         a = { fg = colour.royal, bg = colour.raven, gui = 'bold' },
         b = { fg = colour.slate, bg = colour.stone, gui = 'bold' },
-        c = { fg = colour.metal, bg = 'NONE', gui = 'italic' },
+        c = { fg = colour.stone, bg = 'NONE' },
       },
       insert = { a = { fg = colour.paris, bg = colour.olive, gui = 'bold' } },
       visual = { a = { fg = colour.metal, bg = colour.capri, gui = 'bold' }, },
