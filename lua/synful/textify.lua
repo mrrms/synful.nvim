d2f765ba847c5e1e8ccae4483e8a05081f1e67ff
mrrms <mrrms@icloud.com>
2023-11-23 15:54:09 -0700

    additions

--- lua/synful/textify.lua
+++ lua/synful/textify.lua
@@ -5,23 +5,42 @@ local autocmd = function(pattern, opts)
   vim.api.nvim_create_autocmd(pattern, opts)
 end
 
-function _NeoTxtify()
-  vim.cmd('syntax match textifyTodo "TODO"')
-  vim.cmd('syntax match textifyDone "DONE"')
-  vim.cmd('syntax match textifyBold "\\*.*\\*", conceal')
-  vim.cmd('syntax match textifyItalic "_.*_", conceal')
-  vim.cmd('syntax match textifyComment "•.*$"')
-  vim.cmd('highlight link textifyDone Keyword')
-  vim.cmd('highlight link textifyTodo Todo')
-  vim.cmd('highlight link textifyBold Bold')
-  vim.cmd('highlight link textifyItalic Italic')
-  vim.cmd('highlight link textifyComment Comment')
-end
+local textify = {
+  textifyTodo = { pattern = 'TODO', group = 'Todo' },
+  textifyDone = { pattern = 'DONE', group = 'Keyword' },
+  textifyBold = { pattern = '\\*.*\\*', group = 'Bold' },
+  textifyItalic = { pattern = '_.*_', group = 'Italic' },
+  textifyComment = { pattern = '•.*$', group = 'Comment' },
+  textifyBullet = { pattern = '•', group = 'Todo' },
+  textifyLink = { pattern = 'https?://.*', group = 'Underlined' },
+  textifyTag = { pattern = '@.*', group = 'Tag' },
+  textifyMacro = { pattern = '#.*', group = 'Macro' },
+  textifyNumber = { pattern = '#.*', group = 'Number' },
+  textifyBoolean = { pattern = 'true', group = 'Boolean' },
+  textifyString = { pattern = '"[^"]*"', group = 'String' },
+  textifyFunction = { pattern = 'function', group = 'Function' },
+  textifyReturn = { pattern = 'return', group = 'Return' },
+  textifyCost = { pattern = 'const', group = 'Keyword' },
+  textifyLocal = { pattern = 'local', group = 'luaLocal' },
+}
+
+-- whenever the text is changed, remove the underscores and asterisks from the
+-- text that are used to syntax match for the groups
+autocmd('TextChanged', {
+  pattern = '*.txt',
+  callback = function()
+    vim.cmd(':%s/_//g')
+    vim.cmd(':%s/*//g')
+  end,
+})
 
 -- create an autocmd that uses vim notify when a .txt file is opened
 autocmd('BufEnter', {
   pattern = '*.txt',
   callback = function()
-    _NeoTxtify()
+    for k, v in pairs(textify) do
+      vim.cmd('syntax match ' .. k .. ' "' .. v.pattern .. '"')
+      vim.cmd('highlight link ' .. k .. ' ' .. v.group)
+    end
   end,
 })
