7106d5f5abd7c0f599c45ab20aaf00c211e62028
mrrms <mrrms@icloud.com>
2023-11-23 16:28:39 -0700

    minor code improvements

--- lua/synful/textify.lua
+++ lua/synful/textify.lua
@@ -6,32 +6,32 @@ function _Autocmd(pattern, opts)
 end
 
 local textify = {
-  textifyTodo = { pattern = 'TODO', group = 'Todo' },
-  textifyDone = { pattern = 'DONE', group = 'Keyword' },
-  textifyBold = { pattern = '\\*.*\\*', group = 'Bold' },
-  textifyItalic = { pattern = '_.*_', group = 'Italic' },
-  textifyComment = { pattern = '•.*$', group = 'Comment' },
-  textifyLink = { pattern = 'https?://.*', group = 'Underlined' },
-  textifyTag = { pattern = '@.*', group = 'Tag' },
-  textifyMacro = { pattern = '#.*', group = 'Macro' },
-  textifyNumber = { pattern = '\\d+', group = 'Number' },
-  textifyTrue = { pattern = 'true', group = 'Boolean' },
-  textifyFalse = { pattern = 'false', group = 'Boolean' },
-  textifyString = { pattern = "'[^']*'", group = 'String' },
-  textifyFunction = { pattern = 'function', group = 'Macro' },
-  textifyReturn = { pattern = 'return', group = 'Return' },
-  textifyCost = { pattern = 'const', group = 'Keyword' },
-  textifyLocal = { pattern = 'local', group = 'luaLocal' },
+    textifyTodo = { pattern = 'TODO', group = 'Todo' },
+    textifyDone = { pattern = 'DONE', group = 'Keyword' },
+    textifyBold = { pattern = '\\*.*\\*', group = 'Bold' },
+    textifyItalic = { pattern = '_.*_', group = 'Italic' },
+    textifyComment = { pattern = '•.*$', group = 'Comment' },
+    textifyLink = { pattern = 'https?://.*', group = 'Underlined' },
+    textifyTag = { pattern = '@.*', group = 'Tag' },
+    textifyMacro = { pattern = '#.*', group = 'Macro' },
+    textifyNumber = { pattern = '\\d+', group = 'Number' },
+    textifyTrue = { pattern = 'true', group = 'Boolean' },
+    textifyFalse = { pattern = 'false', group = 'Boolean' },
+    textifyFunction = { pattern = 'function', group = 'Macro' },
+    textifyReturn = { pattern = 'return', group = 'Return' },
+    textifyCost = { pattern = 'const', group = 'Keyword' },
+    textifyLocal = { pattern = 'local', group = 'luaLocal' },
+    textifyString = { pattern = "'[^']*'", group = 'String' },
 }
 
 _Autocmd('BufEnter', {
-  pattern = '*.txt',
-  callback = function()
-    for k, v in pairs(textify) do
-      vim.cmd('syntax match ' .. k .. ' "' .. v.pattern .. '"')
-      vim.cmd('highlight link ' .. k .. ' ' .. v.group)
+    pattern = '*.txt',
+    callback = function()
+        for k, v in pairs(textify) do
+            vim.cmd("syntax match '" .. k .. "' " .. v.pattern)
+            vim.cmd("highlight link " .. k .. " " .. v.group)
+        end
     end
-  end
 })
 
 _Autocmd('TextChanged', {
