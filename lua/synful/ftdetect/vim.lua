9db7d5e22d0954049340cb42579925b60385ee23
mrrms <mrrms@icloud.com>
2023-12-30 15:57:50 -0700

    ftdetect appropriate placement

--- lua/synful/ftdetect/vim.lua
+++ lua/synful/ftdetect/vim.lua
@@ -4,6 +4,6 @@ local groups = {
 }
 
 for item, value in pairs(groups) do
-	vim.cmd("syntax match " .. item .. " /" .. value["string"] .. "/")
-	vim.cmd("highlight link " .. item .. " " .. value["link"])
+	vim.cmd("syntax match " .. item .. " /" .. value.string .. "/")
+	vim.cmd("highlight link " .. item .. " " .. value.link)
 end
