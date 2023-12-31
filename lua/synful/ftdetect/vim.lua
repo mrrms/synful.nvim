3e5103d88dee97ae881e242994156379c5fad285
mrrms <mrrms@icloud.com>
2023-12-31 10:43:34 -0700

    vim.lua custom highlight groups continued

--- lua/synful/ftdetect/vim.lua
+++ lua/synful/ftdetect/vim.lua
@@ -7,3 +7,14 @@ for item, value in pairs(vimCustomHighlights) do
 	vim.cmd("syntax match " .. item .. " /" .. value.pattern .. "/")
 	vim.cmd("highlight link " .. item .. " " .. value.group)
 end
+
+-- Define the custom highlight groups
+vim.cmd("highlight MyCustomGroup1 guifg=#ff0000")
+vim.cmd("highlight MyCustomGroup2 guifg=#00ff00")
+
+-- Apply the custom highlight groups to specific patterns
+vim.cmd("syntax match false /\\<my_pattern1\\>/ containedin=ALL contained")
+vim.cmd("syntax match true /\\<my_pattern2\\>/ containedin=ALL contained")
+
+-- Set the colorscheme
+vim.cmd("colorscheme synful")
