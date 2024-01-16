ebfa20154b82f5dee6b8d56769d9d7e21d850b11
mrrms <mrrms@icloud.com>
2024-01-15 20:27:47 -0700

    string --> not italic

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -54,7 +54,7 @@ local links = {
 	["@keyword.coroutine.typescript"] = "Character",
 	["@exception.typescript"] = "Character",
 	["@include.typescript"] = "Keyword",
-	["@lsp.type.function.lua"] = "Command",
+	["@lsp.type.function.lua"] = "Keyword",
 	["@function.call.bash"] = "TelescopeMatching",
 	["@keyword.operator.lua"] = "Return",
 }
