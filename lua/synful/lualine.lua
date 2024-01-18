164b2b37b43a7c8b34426baa4adfc4ec8245e610
mrrms <mrrms@icloud.com>
2024-01-18 08:47:48 -0700

    ashen --> stone, purps

--- lua/synful/lualine.lua
+++ lua/synful/lualine.lua
@@ -11,12 +11,12 @@ lualine.setup({
 		theme = {
 			normal = {
 				a = { fg = colour.royal, bg = colour.raven, gui = "bold" },
-				b = { fg = colour.ashen, bg = colour.death, gui = "bold" },
+				b = { fg = colour.purps, bg = colour.death, gui = "bold" },
 				c = { fg = colour.stone, bg = "NONE", gui = "italic,bold" },
 			},
 			insert = { a = { fg = colour.paris, bg = colour.green, gui = "bold" } },
 			visual = { a = { fg = colour.capri, bg = colour.steel, gui = "bold" } },
-			replace = { a = { fg = colour.ashen, bg = colour.mauve, gui = "bold" } },
+			replace = { a = { fg = colour.purps, bg = colour.mauve, gui = "bold" } },
 			inactive = {
 				a = { fg = colour.ashen, bg = colour.space },
 				b = { fg = colour.ashen, bg = colour.space },
