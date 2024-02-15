c1af2ba3a6a010011903c11bf2c703181945d175
mrrms <mrrms@icloud.com>
2024-02-15 16:25:06 -0700

    pmenusel --> steel

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -69,7 +69,7 @@ return {
 	Operator = { fg = colour.stone },
 	Pmenu = { fg = colour.pearl },
 	PmenuSbar = { link = "Title" },
-	PmenuSel = { fg = colour.royal, bg = colour.raven, bold = true },
+	PmenuSel = { bg = colour.steel },
 	PmenuThumb = { fg = colour.mauve, bg = "NONE" },
 	PreCondit = { link = "LineNr" },
 	PreProc = { link = "Normal" },
