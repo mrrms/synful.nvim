ddd5ddab08b2fbb86aa1203976d776b2ffa38384
mrrms <mrrms@icloud.com>
2024-02-15 20:16:07 -0700

    pmenusel --> purps

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -69,7 +69,7 @@ return {
 	Operator = { fg = colour.stone },
 	Pmenu = { fg = colour.pearl },
 	PmenuSbar = { link = "Title" },
-	PmenuSel = { fg = colour.pearl, bg = colour.purps },
+	PmenuSel = { fg = colour.pearl, bg = colour.raven, bold = true },
 	PmenuThumb = { fg = colour.mauve, bg = "NONE" },
 	PreCondit = { link = "LineNr" },
 	PreProc = { link = "Normal" },
