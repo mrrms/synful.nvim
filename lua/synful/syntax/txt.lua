9c593b51b1e9ac1d7c98fee864a4877c3f084fe8
mrrms <mrrms@icloud.com>
2024-02-22 10:28:35 -0700

    file structure

--- lua/synful/syntax/txt.lua
+++ lua/synful/syntax/txt.lua
@@ -1,17 +1,20 @@
--- create a table with highlight groups and corresponding regex patterns
+-- Define the highlight groups and corresponding regex patterns
 local groups = {
 	{ "^--.*", "Comment" },
 	{ "^%a[%w_]*", "Identifier" },
 	{ "^%d+%.%d+%.%d+%.%d+", "Number" },
 	{ "^%a[%w_]*%s*%a[%w_]*", "Todo" },
-	-- strings between single quotes
 	{ "^'.*'", "String" },
 	{ '^".*"', "String" },
 }
 
--- iterate through the 'groups' table to apply the highlight groups
+-- Iterate through the 'groups' table to apply the highlight groups
 for _, group in ipairs(groups) do
-	-- syntax match <group[1]> <group[2]>
-	vim.api.nvim_command("syntax match " .. group[2] .. " /" .. group[1] .. "/")
-	vim.api.nvim_set_hl(0, group[1], { link = group[2] })
+	-- Define the autocommand to apply the syntax matching and highlighting
+	vim.cmd("autocmd BufRead,BufNewFile *.txt syntax match " .. group[2] .. " /" .. group[1] .. "/")
+	vim.cmd(
+		"autocmd BufRead,BufNewFile *.txt highlight "
+			.. group[2]
+			.. " guifg=none guibg=none gui=none ctermfg=none ctermbg=none cterm=none"
+	)
 end
