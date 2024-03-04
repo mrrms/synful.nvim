8add362eab8600dd903efc2356aea1e7054a6195
mrrms <mrrms@icloud.com>
2024-03-04 15:23:22 -0700

    comment --> sleet

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -8,7 +8,7 @@ return {
 	luaBrackets = { fg = colour.stone },
 	CodeiumSuggestion = { fg = colour.sleet },
 	Command = { fg = colour.slate, bold = true },
-	Comment = { fg = colour.death, italic = true },
+	Comment = { fg = colour.sleet, italic = true },
 	Conceal = { fg = colour.paris },
 	Conditional = { fg = colour.mauve, italic = true },
 	Constant = { link = "Macro" },
