aa5120cb24a36fe3ee3124c469edaa51c94829e1
mrrms <mrrms@icloud.com>
2024-03-28 11:10:23 -0600

    txt patterns

--- after/syntax/text.lua
+++ after/syntax/text.lua
@@ -1,1 +1,12 @@
-print("the file has been detected")
+local groups = {
+	{ "Macro", "•" },
+	{ "String", "'[^']*" },
+	{ "Comment", "--.*" },
+	{ "Number", "%d+" },
+	{ "Boolean", "true" },
+	{ "Operator", "[%+%-*/]" },
+}
+
+for _, group in ipairs(groups) do
+	vim.cmd("syntax match " .. group[1] .. " '" .. group[2] .. "'")
+end
