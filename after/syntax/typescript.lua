6d7a2901e4c51455d18d548283f2d1614bd081cf
mrrms <mrrms@icloud.com>
2024-03-28 11:03:57 -0600

    loop

--- after/syntax/typescript.lua
+++ after/syntax/typescript.lua
@@ -1,1 +1,8 @@
-vim.cmd("syntax match Boolean '*'")
+local groups = {
+	{ "Boolean", "*" },
+	{ "String", "from" },
+}
+
+for _, group in ipairs(groups) do
+	vim.cmd("syntax match " .. group[1] .. " '" .. group[2] .. "'")
+end
