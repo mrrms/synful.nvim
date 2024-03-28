b7dfee49317c883aa56bfa5a8d8c4c83a893d1eb
mrrms <mrrms@icloud.com>
2024-03-28 10:02:02 -0600

    finally?

--- lua/synful/syntax/ts.lua
+++ lua/synful/syntax/ts.lua
@@ -1,4 +1,1 @@
--- When an index.ts file is opened, detect the character * and link the highlight group to 'Boolean'
--- write this in luascript
-
-vim.cmd("autocmd BufRead,BufNewFile *.ts syntax match Boolean /\\*/")
+vim.cmd("syntax match Boolean '*'")
