640bc63f6696325e4590deebe5184863f2fd6003
mrrms <mrrms@icloud.com>
2024-04-04 14:38:38 -0600

    lualine returns

--- lua/synful/lualine.lua
+++ lua/synful/lualine.lua
@@ -10,20 +10,20 @@ lualine.setup({
 	options = {
 		theme = {
 			normal = {
-				a = { fg = colour.stone },
-				b = { fg = colour.stone },
-				c = { fg = colour.stone },
+				a = { fg = colour.royal, bg = colour.raven },
+				b = { fg = colour.ultra, bg = colour.purps },
+				c = { fg = colour.stone, bg = "NONE" },
 			},
 			insert = {
-				a = { fg = colour.stone },
-				b = { fg = colour.stone },
+				a = { fg = colour.paris, bg = colour.green },
+				b = { fg = colour.raven, bg = colour.stone },
 			},
-			visual = { a = { fg = colour.stone } },
-			replace = { a = { fg = colour.stone } },
+			visual = { a = { fg = colour.capri, bg = colour.stone } },
+			replace = { a = { fg = colour.raven, bg = colour.purps } },
 			inactive = {
-				a = { fg = colour.stone },
-				b = { fg = colour.stone },
-				c = { fg = colour.stone },
+				a = { fg = colour.slate, bg = colour.space },
+				b = { fg = colour.slate, bg = colour.space },
+				c = { fg = colour.space, bg = colour.space },
 			},
 		},
 	},
