369e0a2b661ad8b86aa4acf84d305256ef25270b
mrrms <mrrms@icloud.com>
2024-04-05 08:08:03 -0600

    lualine even darker

--- lua/synful/lualine.lua
+++ lua/synful/lualine.lua
@@ -10,20 +10,20 @@ lualine.setup({
 	options = {
 		theme = {
 			normal = {
-				a = { fg = colour.stone, bg = colour.depth },
+				a = { fg = colour.raven, bg = colour.depth },
 				b = { fg = colour.raven, bg = colour.depth },
-				c = { fg = colour.raven, bg = "NONE" },
+				c = { fg = colour.raven, bg = "none" },
 			},
 			insert = {
-				a = { fg = colour.paris, bg = colour.depth },
+				a = { fg = colour.green, bg = colour.depth },
 				b = { fg = colour.raven, bg = colour.depth },
 			},
 			visual = { a = { fg = colour.capri, bg = colour.depth } },
 			replace = { a = { fg = colour.raven, bg = colour.depth } },
 			inactive = {
-				a = { fg = colour.slate, bg = colour.space },
-				b = { fg = colour.slate, bg = colour.space },
-				c = { fg = colour.space, bg = colour.space },
+				a = { fg = colour.raven, bg = colour.space },
+				b = { fg = colour.raven, bg = colour.space },
+				c = { fg = colour.raven, bg = colour.space },
 			},
 		},
 	},
