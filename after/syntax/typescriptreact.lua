56e7181f1ba07770f2dd59e03117269b0ab3c1e4
mrrms <mrrms@icloud.com>
2024-04-12 11:24:38 -0600

    syntax match ongoin...

--- after/syntax/typescriptreact.lua
+++ after/syntax/typescriptreact.lua
@@ -1,10 +1,9 @@
 local groups = {
-	{ "Boolean", "*" },
-	{ "String", "from" },
-	{ "LineNr", "," },
-	{ "Number", "_" },
+	{ "tsStar", "Boolean", "*" },
+	{ "tsComma", "LineNr", "," },
 }
 
 for _, group in ipairs(groups) do
-	vim.cmd("syntax match " .. group[1] .. " '" .. group[2] .. "'")
+	vim.cmd("syntax match " .. group[1] .. " '" .. group[3] .. "'")
+	vim.cmd("hi def link " .. group[1] .. " " .. group[2])
 end
