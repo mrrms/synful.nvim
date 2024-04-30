3ea2ca2fb4c7187838ebe51aad7e37d6b063edcf
mrrms <mrrms@icloud.com>
2024-04-30 14:25:43 -0600

    Comment --> remove italic

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -9,7 +9,7 @@ return {
 	luaLoop = { link = "Number" },
 	CodeiumSuggestion = { fg = colour.purps },
 	Command = { fg = colour.slate, bold = true },
-	Comment = { fg = colour.ashen, italic = true },
+	Comment = { fg = colour.ashen },
 	Conceal = { fg = colour.paris },
 	Conditional = { fg = colour.mauve, italic = true },
 	Constant = { link = "Macro" },
