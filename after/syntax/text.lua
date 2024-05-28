2ae0c8b1819b702fddb208e4e1d0dba7e1e26130
mrrms <mrrms@icloud.com>
2024-05-28 10:37:01 -0600

    txtWarn --> backtick conceal

--- after/syntax/text.lua
+++ after/syntax/text.lua
@@ -21,6 +21,22 @@ for _, group in ipairs(groups) do
 	vim.api.nvim_set_hl(0, group[1], { link = group[3] })
 end
 
+local function highlight_backticks()
+	local pattern = "`([^`]+)`"
+	local highlight_group = "txtWarn"
+
+	vim.api.nvim_command("syntax match txtBackticks /`[^`]*`/ conceal cchar=")
+	vim.api.nvim_command("highlight link txtBackticks " .. highlight_group)
+
+	vim.api.nvim_command("syntax match txtBackticks /`[^`]*`/ contains=txtBackticksConcealed")
+	vim.api.nvim_command("syntax match txtBackticksConcealed /`/ contained conceal cchar=")
+	vim.api.nvim_command("syntax match txtBackticksConcealed /`/ contained conceal cchar=")
+
+	vim.api.nvim_command("highlight link txtBackticksConcealed Ignore")
+end
+
+vim.api.nvim_create_augroup("backtick_highlight", { clear = true })
+
 -- vim.cmd('syntax region txtQuotes start=+"+ end=+"+')
 -- vim.cmd("syntax region txtTernary start=+`+ end=+`+")
 -- vim.cmd("syntax region txtSingleQuotes start=+'+ end=+'+")
