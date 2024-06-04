a4fdb764d73dc33ea899564992a0bc707105cd87
mrrms <mrrms@icloud.com>
2024-06-03 20:45:54 -0600

    Pmenusel

--- lua/synful/groups.lua
+++ lua/synful/groups.lua
@@ -55,7 +55,7 @@ return {
 	NotifyWARNIcon = { fg = colour.peach },
 	NotifyWARNTitle = { fg = colour.peach },
 	Pmenu = { fg = colour.slate },
-	PmenuSel = { bg = colour.steel },
+	PmenuSel = { bg = colour.steel, fg = colour.capri },
 	PmenuSbar = { fg = colour.steel, bg = colour.depth },
 	PmenuThumb = { fg = colour.space, bg = colour.space },
 	PreProc = { fg = colour.slate },
